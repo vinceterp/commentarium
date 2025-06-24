5010e18187bc2ca0a3802e0c9e822f6f5127bef2
Paul Campbell <vinceterp@outlook.com>
2025-06-24 16:38:26 -0400

    fix permission enum

--- src/main/java/com/commentarium/entities/Permission.java
+++ src/main/java/com/commentarium/entities/Permission.java
@@ -10,10 +10,10 @@ public enum Permission {
     ADMIN_UPDATE("admin:update"),
     ADMIN_CREATE("admin:create"),
     ADMIN_DELETE("admin:delete"),
-    MANAGER_READ("management:read"),
-    MANAGER_UPDATE("management:update"),
-    MANAGER_CREATE("management:create"),
-    MANAGER_DELETE("management:delete")
+    MANAGER_READ("moderator:read"),
+    MANAGER_UPDATE("moderator:update"),
+    MANAGER_CREATE("moderator:create"),
+    MANAGER_DELETE("moderator:delete")
 
     ;
 
